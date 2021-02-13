7373b63c772664cb06e02e4724171dc471fc5a12
Ragy Costa de jesus <rdcostadejesus4087@eagle.fgcu.edu>
2021-02-13 12:54:24 -0500

    Initial Commit

---
+++ autoLoan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  autoLoan
+//
+//  Created by Ragy Costa de jesus on 2/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
